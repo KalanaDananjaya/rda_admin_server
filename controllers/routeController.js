9e2caf56961cfff9555e3f93db0ea0154bd4529b
Kalana <kalana.16@cse.mrt.ac.lk>
2019-02-20 21:42:45 +0530

    refactored code to show same format response message

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -1,5 +1,7 @@
 const loginInfo=require("../models/login_info");
 const personalInfo = require("../models/personal_info");
+const bcrypt = require('bcrypt');
+
 //const superAdmin = require("../models/super_admin");
 
 const uuidv1 = require('uuid/v1');
@@ -7,30 +9,56 @@ const uuidv1 = require('uuid/v1');
 exports.listPersonalInfo = (req,res) => {
     personalInfo.find({},(err,info)=>{
         if (err) {
-            res.status(500).send(err);
+            let msg = {
+                success : false,
+                msg : err
+            }
+            res.status(500).json(msg);
           }
-          console.log(info);
-          res.status(200).json(info);
+          else{
+            let msg = {
+                success : true,
+                msg : info
+            }
+                res.status(200).json(msg);
+        }
     });
 };
 
 exports.listUserInfo = (req,res) => {
     personalInfo.find({uid:req.body.uid},(err,info)=>{
         if (err) {
-            res.status(500).send(err);
+            let msg = {
+                success : false,
+                msg : err
+            }
+            res.status(500).json(msg);
           }
-          console.log(info);
-          res.status(200).json(info);
+        else{
+          let msg = {
+            success : true,
+            msg : info
+        }
+            res.status(200).json(msg);
+        }
     });
 };
 
 exports.listPendingUsers = (req,res) => {
     personalInfo.find({status : "pending"},(err,info)=>{
         if (err) {
-            res.status(500).send(err);
+            let msg = {
+                success : false,
+                msg : err
+            }
+            res.status(500).json(msg);
           }
         else{
-            res.status(200).json(info);
+            let msg = {
+                success : true,
+                msg : info
+            }
+            res.status(200).json(msg);
         }
     });
 };
@@ -38,10 +66,18 @@ exports.listPendingUsers = (req,res) => {
 exports.listApprovedUsers = (req,res) => {
     personalInfo.find({status : "approved"},(err,info)=>{
         if (err) {
-            res.status(500).send(err);
+            let msg = {
+                success : false,
+                msg : err
+            }
+            res.status(500).json(msg);
           }
         else{
-            res.status(200).json(info);
+            let msg = {
+                success : true,
+                msg : info
+            }
+            res.status(200).json(msg);
         }
     });
 };
@@ -49,10 +85,18 @@ exports.listApprovedUsers = (req,res) => {
 exports.listRejectedUsers = (req,res) => {
     personalInfo.find({status : "rejected"},(err,info)=>{
         if (err) {
-            res.status(500).send(err);
+            let msg = {
+                success : false,
+                msg : err
+            }
+            res.status(500).json(msg);
           }
         else{
-            res.status(200).json(info);
+            let msg = {
+                success : true,
+                msg : info
+            }
+            res.status(200).json(msg);
         }
     });
 };
@@ -74,48 +118,86 @@ exports.createUser = (req,res) => {
     });
     user.save(function (err){
         if(err){
-            return err;
+            let msg = {
+                success : false,
+                msg : err
+            }
+            res.status(500).json(msg);
         }
         else{
-            res.status(200).json("user request created successfully");
+            //save to login details
+            var password = req.body.password;
+            const saltRounds = 10;
+            
+            console.log(password);
+            bcrypt.hash(password, saltRounds).then(function(hash) {
+                let user_login = new loginInfo({
+                    email : req.body.email,
+                    uid : generatedUid,
+                    password :hash,
+                    approvalStatus : "pending",
+                    user_type : req.body.user_type
+                });
+        
+                user_login.save(function (err){
+                    if(err){
+                        let msg = {
+                            success : false,
+                            msg : err
+                        }
+                        res.status(500).json(msg);
+                    }
+                    else{
+                        let msg = {
+                            success : true,
+                            msg : "success"
+                        }
+                        res.status(200).json(msg);
+                    }
+                });
+            });
+
+           
+
+            
         }
     });
 
-    //save to login details
-    let user_login = new loginInfo({
-        email : req.body.email,
-        uid : generatedUid,
-        password : req.body.password,
-        approvalStatus : "pending",
-        user_type : req.body.user_type
-    });
-
-    user_login.save(function (err){
-        if(err){
-            return err;
-        }
-        else{
-            res.status(200).json("user login info added successfully");
-        }
-    });
+    
 };
 
 exports.approveUser = (req,res)=> {
     personalInfo.findOneAndUpdate({uid:req.body.uid},{approvalStatus : "approved"},{new : true},(err,info)=>{
         if (err) {
-            res.status(500).send(err);
+            let msg = {
+                success : false,
+                msg : err
+            }
+            res.status(500).json(msg);
           }
         else{
-            res.status(200).json(info);
+            let msg = {
+                success : true,
+                msg : "success"
+            }
+            res.status(200).json(msg);
         }
     });
 
     loginInfo.findOneAndUpdate({uid:req.body.uid},{approvalStatus : "approved"},{new : true},(err,info)=>{
         if (err) {
-            res.status(500).send(err);
+            let msg = {
+                success : false,
+                msg : err
+            }
+            res.status(500).json(msg);
           }
         else{
-            res.status(200);
+            let msg = {
+                success : true,
+                msg : "success"
+            }
+            res.status(200).json(msg);
         }
     });
 };
@@ -123,19 +205,35 @@ exports.approveUser = (req,res)=> {
 exports.rejectUser = (req,res)=> {
     personalInfo.findOneAndUpdate({uid:req.body.uid},{approvalStatus : "rejected"},{new : true},(err,info)=>{
         if (err) {
-            res.status(500).send(err);
+            let msg = {
+                success : false,
+                msg : err
+            }
+            res.status(500).json(msg);
           }
         else{
-            res.status(200).json(info);
+            let msg = {
+                success : true,
+                msg : "success"
+            }
+            res.status(200).json(msg);
         }
     });
 
     loginInfo.findOneAndUpdate({uid:req.body.uid},{approvalStatus : "rejected"},{new : true},(err,info)=>{
         if (err) {
-            res.status(500).send(err);
+            let msg = {
+                success : false,
+                msg : err
+            }
+            res.status(500).json(msg);
           }
         else{
-            res.status(200);
+            let msg = {
+                success : true,
+                msg : "success"
+            }
+            res.status(200).json(msg);
         }
     });
 };
@@ -143,15 +241,35 @@ exports.rejectUser = (req,res)=> {
 exports.deleteUser = (req, res) => {
     personalInfo.remove({ uid: req.body.uid }, (err, info) => {
       if (err) {
-        res.status(404).send(err);
+        let msg = {
+            success : false,
+            msg : err
+        }
+        res.status(404).json(msg);
       }
-      res.status(200).json({ message: "User succesfully deleted" });
+      else{
+        let msg = {
+            success : true,
+            msg : "success"
+        }
+        res.status(200).json(msg);
+      }
     });
 
     loginInfo.remove({ uid: req.body.uid }, (err, info) => {
         if (err) {
-          res.status(404).send(err);
+            let msg = {
+                success : false,
+                msg : err
+            }
+            res.status(404).json(msg);
         }
-        res.status(200);
+        else{
+            let msg = {
+                success : true,
+                msg : "success"
+            }
+            res.status(200).json(msg);
+        }
       });
   };
--- documentation/user/getAllUsers.md
+++ documentation/user/getAllUsers.md
@@ -20,12 +20,27 @@ Get list of all users registered in the system
 
 
 ```json
-{
-    "success": true,
-    "msg": [
-        "cs2000",
-        "cs2001",
-        "cs2002"
-    ]
-}
+[
+    {
+        "category": [
+            "dop"
+        ],
+        "approvalStatus": [
+            "pending"
+        ],
+        "user_type": [
+            "user"
+        ],
+        "_id": "5c68f01c99bbc7346c7a6463",
+        "email": "kalana.16@cse.mrt.ac.lk",
+        "uid": "555",
+        "telephone": 774052085,
+        "fname": "kalana",
+        "lname": "wijethunga",
+        "__v": 0
+    },
+    {
+        ...
+    }
+]
 ```
